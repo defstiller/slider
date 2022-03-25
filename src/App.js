125641a6085230dede7066a1733cccdf8d0d6c46
defstiller <81063003+defstiller@users.noreply.github.com>
2022-03-25 12:12:45 -0500

    completed project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import {useState, useEffect, useRef} from "react"
+import Review from "./Review"
+import data from "./data"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [index, setIndex] = useState(3)
+  let dataLength = data.length
+  const [currentReview, setReview] = useState(data[dataLength - index])
+  useEffect(() => {
+    if(index > dataLength) {
+      setIndex(1)
+    } else if (index <= 0) {
+      setIndex(dataLength)
+    }else {
+      setReview(data[dataLength - index])
+    }
+  }, [index])
+  return ( <>
+  <div className="App">
+      <h1>Reviews</h1>
+      <main>
+        <Review key={currentReview.id} {...currentReview}/>
+      </main>
+      <button onClick={() => setIndex(index + 1)}>Previous</button>
+      <button onClick={() => setIndex(index - 1)}>Next</button>
     </div>
-  );
+  </>);
 }
 
 export default App;
